ea55658d3824b2fcc528e01b9cbdf424af2601d0
Dennis Kamau <dkamau476@gmail.com>
2023-01-23 06:08:57 -0800

    corrections

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,16 +7,21 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, j, swapped = 0;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
+		{
 			if (array[j] > array[j + 1])
 			{
 				swap(&array[j], &array[j + 1]);
+				swapped++;
 				print_array(array, size);
 			}
+		}
+		if (swapped == 0)
+			return;
 	}
 }
 /**
